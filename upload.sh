cb312fda2aa82bfb1203ed77f86bbbad5e50cfb3
Shuaiwen-Cui <SHUAIWEN001@e.ntu.edu.sg>
2024-08-18 12:21:48 +0800

    auto commit by win-upload.bat

---
+++ upload.sh
@@ -0,0 +1,23 @@
+# usage: in git bash, input 'sh update.sh' - windows; or in terminal input './update.sh' and click approve git for windows pop up to run it
+# usage: in terminal, input 'bash update.sh' to run it
+# usage: note that the quotation marks should not be input into the terminal
+
+echo '--------upload files start--------'   
+# enter the target folder
+# cd ./
+
+# git init
+git add .
+git status
+git commit -m 'auto update Infinity'
+echo '--------commit successfully--------'
+
+git push -f https://github.com/Shuaiwen-Cui/MCU_NODE_STM32.git main
+# git push -u https://github.com/Shuaiwen-Cui/MCU_NODE_STM32.git main
+# git remote add origin https://github.com/Shuaiwen-Cui/MCU_NODE_STM32.git
+# git push -u origin main
+echo '--------push to GitHub successfully--------'
+
+cd ./MCU_DOC/
+mkdocs gh-deploy
+echo '--------deployed to Github Pages sucessfully--------'
